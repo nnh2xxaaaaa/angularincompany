6db2fbbbe696e6275600b24ab0d4ac956b418889
nnh2xxaaaaa <ngonhathuy6878@gmail.com>
2023-08-22 18:01:51 +0700

    add miss data

--- src/app/layout-user/layout-user.component.ts
+++ src/app/layout-user/layout-user.component.ts
@@ -4,7 +4,7 @@ import { UntypedFormGroup } from '@angular/forms';
 import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 import { AppService } from '../app.server';
 import { RootUsers } from '../interface/user.interface';
-import { FormGroup , FormBuilder ,Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-layout-user',
@@ -24,7 +24,10 @@ export class LayoutUserComponent implements OnInit {
   innerAddress: string | undefined = '';
   innerCompany: string | undefined = '';
   innerReset: string = '';
-  changeEmail : string  = '';
+  changeEmail: string = '';
+  review: string = '';
+  login: string = '';
+  addNewUser : string = '';
   @Input() name: any;
 
   constructor(
@@ -32,14 +35,16 @@ export class LayoutUserComponent implements OnInit {
     private http: HttpClient,
     private fb: FormBuilder
   ) {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      // Add other form controls here
-    });
+    setInterval(() => {
+        if(this.users.length  >  10) {
+            this.review = 'disabled'
+            this.login = 'Khong the dang ki'
+        }else {
+          this.review = ''
+          this.login = 'co the dang ki'
+        }
+    }, 1000);
   }
-
-
 
   ngOnInit() {
     this.http
@@ -65,6 +70,10 @@ export class LayoutUserComponent implements OnInit {
     this.innerCompany = undefined;
   }
 
+  ss() {
+    alert('Quickly clicked'); 
+  }
+
   isVisible = false;
 
   showModal(): void {
@@ -73,15 +82,16 @@ export class LayoutUserComponent implements OnInit {
 
   handleOk(): void {
     console.log('Button ok clicked!');
-    const userToUpdate = this.users.find((user) => user.name === this.innerName);
+    const userToUpdate = this.users.find(
+      (user) => user.name === this.innerName
+    );
     if (userToUpdate) {
       this.innerName = this.changeEmail;
-      userToUpdate.name = this.changeEmail; 
+      userToUpdate.name = this.changeEmail;
     }
 
-    console.log(userToUpdate)
-    console.log(this.users)
-
+    console.log(userToUpdate);
+    console.log(this.users);
   }
 
   handleCancel(): void {
@@ -93,5 +103,33 @@ export class LayoutUserComponent implements OnInit {
     this.innerName = undefined;
     this.innerAddress = undefined;
     this.innerCompany = undefined;
+  }
+
+  addUser(){
+      if(this.addNewUser.length!=0){
+        this.users.push({
+          "id": 1,
+          "name": `${this.addNewUser}`,
+          "username": "Bret",
+          "email": "Sincere@april.biz",
+          "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+              "lat": "-37.3159",
+              "lng": "81.1496"
+            }
+          },
+          "phone": "1-770-736-8031 x56442",
+          "website": "hildegard.org",
+          "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+          }
+        })
+      }
   }
 }
